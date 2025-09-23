0cc409af8de2a094af39e32551a38ba1b4568bed
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-09-23 16:07:08 -0300

    criando classe Usuario

---
+++ backend/models/usuarios.py
@@ -0,0 +1,56 @@
+from database.banco_dados_usuarios import conectar_banco_dados_usuarios
+
+class Usuario:
+    def __init__(self, nome_completo, senha):
+        self.nome_completo = nome_completo
+        self.senha = senha
+
+    def inserir_usuario(self):
+        conexao = conectar_banco_dados_usuarios()
+        cursor = conexao.cursor()
+
+        cursor.execute(
+        """
+        INSERT INTO TabelaUsuarios (
+        nome_completo, senha
+        )
+        VALUES (?, ?)
+        """, (self.nome_completo, self.senha)
+        )
+
+        conexao.commit()
+        conexao.close()
+
+    def excluir_usuario(self):
+        conexao = conectar_banco_dados_usuarios()
+        cursor = conexao.cursor()
+
+        cursor.execute(
+        """
+        DELETE FROM TabelaUsuarios
+        WHERE nome_completo = ? AND senha = ?
+        """, (self.nome_completo, self.senha)
+        )
+
+        conexao.commit()
+        conexao.close()
+
+    @staticmethod
+    def buscar_usuario(nome_completo):
+        conexao = conectar_banco_dados_usuarios()
+        cursor = conexao.cursor()
+
+        cursor.execute(
+        """
+        SELECT 1 FROM TabelaUsuarios
+        WHERE nome_completo = ?
+        VALUES (?)
+        """, (nome_completo,)
+        )
+
+        conexao.commit()
+        conexao.close()
+
+        resultado = cursor.fetchone()
+
+        return resultado
