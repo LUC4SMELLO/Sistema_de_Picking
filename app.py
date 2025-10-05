5336d3e15735aa8b4e4f9d3cb7f75803ec03fb72
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-10-05 19:27:53 -0300

    nova rota 'picking'

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from routes.homepage import homepage_bp
 from routes.login import login_bp
 from routes.cadastro import cadastro_bp
+from routes.picking import picking_bp
 
 app = Flask(__name__)
 app.secret_key = "secret_key"
@@ -11,6 +12,7 @@ app.secret_key = "secret_key"
 app.register_blueprint(homepage_bp)
 app.register_blueprint(login_bp)
 app.register_blueprint(cadastro_bp)
+app.register_blueprint(picking_bp)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
