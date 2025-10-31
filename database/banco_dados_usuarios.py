111c4b4b21fa41843d2bfe973985e52d0407a494
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-10-31 20:42:25 -0300

    atualizando banco de dados

--- database/banco_dados_usuarios.py
+++ database/banco_dados_usuarios.py
@@ -1,16 +1,19 @@
 import sqlite3
 
+from backend.constantes.banco_dados import BANCO_DADOS_USUARIOS, TABELA_USUARIOS
+
 def conectar_banco_dados_usuarios():
 
-    return sqlite3.connect("TabelaUsuarios.db")
+    return sqlite3.connect(BANCO_DADOS_USUARIOS)
 
 def criar_banco_dados_usuarios():
     conexao = conectar_banco_dados_usuarios()
     cursor = conexao.cursor()
 
     cursor.execute(
-    """
-    CREATE TABLE IF NOT EXISTS TabelaUsuarios (
+    f"""
+    CREATE TABLE IF NOT EXISTS {TABELA_USUARIOS} (
+    usuario_id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome_completo VARCHAR(50),
     senha VARCHAR(15)
     )
