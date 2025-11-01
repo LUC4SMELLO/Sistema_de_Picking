0e8ba0b6191af438f8aadab80d0312dd593da552
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-11-01 14:02:16 -0300

    registrando Blueprint caminhoes

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from routes.login import login_bp
 from routes.cadastro import cadastro_bp
 from routes.picking import picking_bp
 from routes.upload import upload_bp
+from routes.caminhoes import caminhoes_bp
+
+
 
 app = Flask(__name__)
 app.secret_key = "secret_key"
@@ -15,6 +18,8 @@ app.register_blueprint(login_bp)
 app.register_blueprint(cadastro_bp)
 app.register_blueprint(picking_bp)
 app.register_blueprint(upload_bp)
+app.register_blueprint(caminhoes_bp)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
