a9e72b78454854ffaf6e37dc73b6a01d6de3bb09
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-11-01 14:01:45 -0300

    função para normalizar arquivo CARGA.csv

--- backend/scripts/arquivo_carga.py
+++ backend/scripts/arquivo_carga.py
@@ -1,24 +1,25 @@
 import pandas as pd
 
-df = pd.read_csv("CARGA.csv", sep=";", encoding="latin1", index_col=False)
+def agrupar_produtos_por_caminhao(caminho_arquivo):
+  df = pd.read_csv(caminho_arquivo, sep=";", encoding="latin1", index_col=False)
 
-df.columns = df.columns.str.strip()
+  df.columns = df.columns.str.strip()
 
-# NORMALIZA COLUNAS IMPORTANTES
-df["MAPA"] = df["MAPA"].astype(str).str.strip()
-df["BAIA"] = df["BAIA"].astype(str).str.strip()
-df["PRODUTO"] = df["PRODUTO"].astype(str).str.strip()
-df["QUANTIDADE"] = df["QUANTIDADE"].astype(str).str.strip()
+  # NORMALIZA COLUNAS IMPORTANTES
+  df["MAPA"] = df["MAPA"].astype(str).str.strip()
+  df["BAIA"] = df["BAIA"].astype(str).str.strip()
+  df["PRODUTO"] = df["PRODUTO"].astype(str).str.strip()
+  df["QUANTIDADE"] = df["QUANTIDADE"].astype(str).str.strip()
 
-df["QUANTIDADE"] = pd.to_numeric(df["QUANTIDADE"], errors="coerce")
+  df["QUANTIDADE"] = pd.to_numeric(df["QUANTIDADE"], errors="coerce")
 
-# AGRUPA POR CAMINHÃO (MAPA, BAIA E PRODUTO)
-agrupado = (
-    df.groupby(["MAPA", "BAIA", "PRODUTO"], as_index=False)
-      .agg({"QUANTIDADE": "sum"})
-)
+  # AGRUPA POR CAMINHÃO (MAPA, BAIA E PRODUTO)
+  agrupado = (
+      df.groupby(["MAPA", "BAIA", "PRODUTO"], as_index=False)
+        .agg({"QUANTIDADE": "sum"})
+  )
 
 
-agrupado = agrupado.sort_values(["MAPA", "BAIA"])
+  agrupado = agrupado.sort_values(["MAPA", "BAIA"])
 
-print(agrupado)
+  return agrupado
